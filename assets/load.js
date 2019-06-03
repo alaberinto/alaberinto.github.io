237a171f5bbb7f76eb188792f5eea8feea5092ba
Alvin Laberinto <alvinlaberinto@rocketmail.com>
2019-06-03 00:18:42 -0600

    Trying to add preload JS.

--- assets/load.js
+++ assets/load.js
@@ -1,6 +1,9 @@
-var section = document.querySelector("section");
-document.body.classList.add('js-loading');
-section.addEventListener("load", removeLoadingClass);
-function removeLoadingClass() {
-  document.body.classList.remove('js-loading');
-}
+window.onload = function () {
+
+	setTimeout(function () {
+		// preload image
+		new Image().src = "images/background_max.png";
+        new Image().src = "images/background_max2.png";
+	}, 1000);
+
+};

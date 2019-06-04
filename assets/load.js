b7ff9749448989d4f1e38904208c824e8aa52ba7
Alvin Laberinto <alvinlaberinto@rocketmail.com>
2019-06-04 00:44:22 -0600

    Rewrote index.html in Mithril.js

--- assets/load.js
+++ assets/load.js
@@ -1,3 +1,54 @@
-function menu() {
-    alert("hi");
+var root = document.body
+
+var Hamburg = {
+    view: function() {
+        return m("figure", {class: "navbar-item image has-text-black center"}, m("i", {class:"fas fa-bars"})
+    )}
 }
+
+var Contact = {
+    view: function() {
+        return m("a", {href: "/contact", class: "navbar-item is-transparent desktop"
+        }, "CONTACT")
+    }
+}
+
+var ColumnRight = {
+    view: function() {
+        return m("div", {class: "column right heading has-text-weight-bold has-text-black"}, [m(Contact), m(Hamburg)]
+    )}
+}
+
+var Name = {
+    view: function() {
+        return m("div", {class: "name"}, "Alvin Laberinto")
+    }
+}
+
+var ColumnLeft = {
+    view: function() {
+        return m("div", {class: "column left has-text-black"}, m(Name)
+    )}
+}
+
+var Columns = {
+    view: function() {
+        return m("div",
+                 {class: "columns is-mobile is-marginless"},
+                 [m(ColumnLeft), m(ColumnRight)]
+    )}
+}
+
+var Hero = {
+    view: function() {
+        return m("div", {class: "hero-head"}, m(Columns)
+    )}
+}
+
+var Section = {
+    view: function() {
+        return m("section", {class: "hero is-fullheight animated fadeInUp delay-1s"}, m(Hero)
+    )}
+}
+
+m.mount(root, Section)

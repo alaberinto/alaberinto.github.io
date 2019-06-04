5ed73a60e956fb265648c694a154896af004bf05
Alvin Laberinto <alvinlaberinto@rocketmail.com>
2019-06-04 01:09:04 -0600

    Removed contact.html and added routing!
    Routing now goes to contact page. No longer multiple HTMLs, but instead 1 SPA

--- assets/load.js
+++ assets/load.js
@@ -1,54 +1,81 @@
 var root = document.body
 
 var Hamburg = {
-    view: function() {
-        return m("figure", {class: "navbar-item image has-text-black center"}, m("i", {class:"fas fa-bars"})
-    )}
+    view: function () {
+        return m("figure", {
+            class: "navbar-item image has-text-black center"
+        }, m("i", {
+            class: "fas fa-bars"
+        }))
+    }
 }
 
-var Contact = {
-    view: function() {
-        return m("a", {href: "/contact", class: "navbar-item is-transparent desktop"
+var ContactLink = {
+    view: function () {
+        return m("a[href=/contact]", {
+            class: "navbar-item is-transparent desktop",
+            oncreate: m.route.link
         }, "CONTACT")
     }
 }
 
 var ColumnRight = {
-    view: function() {
-        return m("div", {class: "column right heading has-text-weight-bold has-text-black"}, [m(Contact), m(Hamburg)]
-    )}
+    view: function () {
+        return m("div", {
+            class: "column right heading has-text-weight-bold has-text-black"
+        }, [m(ContactLink), m(Hamburg)])
+    }
 }
 
 var Name = {
-    view: function() {
-        return m("div", {class: "name"}, "Alvin Laberinto")
+    view: function () {
+        return m("div", {
+            class: "name"
+        }, "Alvin Laberinto")
     }
 }
 
 var ColumnLeft = {
-    view: function() {
-        return m("div", {class: "column left has-text-black"}, m(Name)
-    )}
+    view: function () {
+        return m("div", {
+            class: "column left has-text-black"
+        }, m(Name))
+    }
 }
 
 var Columns = {
-    view: function() {
-        return m("div",
-                 {class: "columns is-mobile is-marginless"},
+    view: function () {
+        return m("div", {
+                class: "columns is-mobile is-marginless"
+            },
                  [m(ColumnLeft), m(ColumnRight)]
-    )}
+        )
+    }
 }
 
 var Hero = {
-    view: function() {
-        return m("div", {class: "hero-head"}, m(Columns)
-    )}
+    view: function () {
+        return m("div", {
+            class: "hero-head"
+        }, m(Columns))
+    }
 }
 
-var Section = {
-    view: function() {
-        return m("section", {class: "hero is-fullheight animated fadeInUp delay-1s"}, m(Hero)
-    )}
+var Home = {
+    view: function () {
+        return m("section", {
+            class: "hero is-fullheight animated fadeInUp delay-1s"
+        }, m(Hero))
+    }
 }
 
-m.mount(root, Section)
+var Contact = {
+    view: function () {
+        return "Hello!"
+    }
+}
+
+m.route(root, "/", {
+    "/": Home,
+    "/contact": Contact,
+})

290a7b26d3ff7a4d6a6dc77a546b091b94b70372
Alvin Laberinto <alvinlaberinto@rocketmail.com>
2019-06-03 18:10:46 -0600

    Beginning work on modal menu

--- assets/load.js
+++ assets/load.js
@@ -1,9 +1,3 @@
-window.onload = function () {
-
-	setTimeout(function () {
-		// preload image
-		new Image().src = "images/background_max.png";
-        new Image().src = "images/background_max2.png";
-	}, 2000);
-
-};
+function menu() {
+    alert("hi");
+}
